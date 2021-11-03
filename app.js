465f21e334141d4ab7c2bfece928883a687149f7
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-11-02 19:55:08 -0500

    se agrego firebase a la app

--- app.js
+++ app.js
@@ -2,6 +2,26 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGRVeMqscc6VFC84R3CbHfpH6j_0wM6QQ",
+  authDomain: "test-fb-mintic.firebaseapp.com",
+  projectId: "test-fb-mintic",
+  storageBucket: "test-fb-mintic.appspot.com",
+  messagingSenderId: "159181197302",
+  appId: "1:159181197302:web:899c84b982022f000d41d7",
+  measurementId: "G-ZZTPH7VXZ5"
+};
+
+// Initialize Firebase
+const FBapp = initializeApp(firebaseConfig);
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
