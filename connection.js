e197235fa264d0fffedcef64d348c083c7e57f31
Geiner <davidlararada1062@hotmail.com>
2021-11-06 14:01:27 -0500

    se configuro las variables de entorno

--- connection.js
+++ connection.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
-const database = require("./database");
+// const database = require("./database");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 };
 
-let connection = mongoose.connect(database.url, options).then(
+let connection = mongoose.connect(process.env.MONGODB_URI, options).then(
   () => console.log("Database connected funciono "),
   (error) => console.error("Database could't be connected to: " + error)
 );
--- database.js
+++ database.js
@@ -1,9 +1,9 @@
-const user = 'user123';
-const password = '12345';
-const ddbb = 'proyecto-ciclo-3';
-const url = `mongodb+srv://${user}:${password}@cluster0.tifmp.mongodb.net/${ddbb}?retryWrites=true&w=majority`;
+// const user = 'user123';
+// const password = '12345';
+// const ddbb = 'proyecto-ciclo-3';
+// const url = `mongodb+srv://${user}:${password}@cluster0.tifmp.mongodb.net/${ddbb}?retryWrites=true&w=majority`;
 
-module.exports = { 
-  url
-  // db: "mongodb://localhost:27017/products", 
-}; 
+// module.exports = {
+//   url
+//   // db: "mongodb://localhost:27017/products",
+// };
