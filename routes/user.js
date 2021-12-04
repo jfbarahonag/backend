118e876dcd08c3fa5570e535018da1bc35587f92
Juan Felipe Barahona Gonzalez <jfbarahonag@unal.edu.co>
2021-12-04 01:41:46 -0500

    se actualizo la base de datos para modificar la vista de usuarios de admin

--- routes/user.js
+++ routes/user.js
@@ -9,4 +9,8 @@ router.post("/add", UserController.add);
 // publico
 router.post("/login", UserController.login);
 
+router.get("/", UserController.index);
+
+router.put("/update/:id", UserController.update);
+
 module.exports = router;
